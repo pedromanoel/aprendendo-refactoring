69a6908242b3fa1e659a557dd7b32b9a50269423
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-05-13 00:04:39 -0300

    Adiciona o código de exemplo

---
+++ src/main/java/Customer.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+
+        String result = "Rental record for " + getName() + "\n";
+        for (Rental rental : rentals) {
+            double amount = 0;
+            switch (rental.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    amount += 2;
+                    if (rental.getDaysRented() > 2)
+                        amount += (rental.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    amount += rental.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    amount += 1.5;
+                    if (rental.getDaysRented() > 3)
+                        amount += (rental.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+                frequentRenterPoints++;
+
+            // show figures for this rental
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+
+            totalAmount += amount;
+        }
+        // add footer lines
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+}
