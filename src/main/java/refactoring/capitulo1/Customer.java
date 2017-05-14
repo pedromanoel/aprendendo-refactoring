95cf3a2debec3fbda1c3b7d8f7caffc53f53e4c8
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-05-13 23:56:15 -0300

    Move cálculo de pontuação para Rental

--- src/main/java/refactoring/capitulo1/Customer.java
+++ src/main/java/refactoring/capitulo1/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
         String result = "refactoring.capitulo1.Rental record for " + getName() + "\n";
         for (Rental each : rentals) {
             // add frequent renter points
-            frequentRenterPoints += frequentRenterPointsFor(each);
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this each
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
@@ -37,16 +37,5 @@ public class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
-    }
-
-    private int frequentRenterPointsFor(Rental each) {
-        int frequentRenterPoints = 0;
-        // add frequent renter points
-        frequentRenterPoints++;
-        // add bonus for a two day new release each
-        if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
-            frequentRenterPoints++;
-
-        return frequentRenterPoints;
     }
 }
--- src/main/java/refactoring/capitulo1/Rental.java
+++ src/main/java/refactoring/capitulo1/Rental.java
@@ -38,4 +38,15 @@ public class Rental {
 
         return result;
     }
+
+    public int getFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        // add frequent renter points
+        frequentRenterPoints++;
+        // add bonus for a two day new release each
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+            frequentRenterPoints++;
+
+        return frequentRenterPoints;
+    }
 }
