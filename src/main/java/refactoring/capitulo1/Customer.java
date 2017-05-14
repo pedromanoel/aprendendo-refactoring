247d4e82681297a4a1882ad215a84b2e75a64a02
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-05-13 23:28:25 -0300

    Move cálculo do valor do aluguel para Rental

--- src/main/java/refactoring/capitulo1/Customer.java
+++ src/main/java/refactoring/capitulo1/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
 
         String result = "refactoring.capitulo1.Rental record for " + getName() + "\n";
         for (Rental each : rentals) {
-            double amount = amountFor(each);
+            double amount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -41,27 +41,6 @@ public class Customer {
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
-    }
-
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
         return result;
     }
 }
