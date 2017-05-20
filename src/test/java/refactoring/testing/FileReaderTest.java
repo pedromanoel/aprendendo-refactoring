3993e0d8652a35dcd4812fcb32f14bb74cea3266
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-05-20 13:50:35 -0300

    Adiciona primeiro caso de teste
    Nesse caso ele fala de alterar o teste para ver quebrar e se certificar
    de que o teste realmente está funcionando.

---
+++ src/test/java/refactoring/testing/FileReaderTest.java
@@ -0,0 +1,40 @@
+package refactoring.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileReaderTest {
+
+    private InputStream file;
+
+    @Before
+    public void setUp() {
+        file = loadFixtureFile();
+    }
+
+    @Test
+    public void testRead() throws IOException {
+        char ch = '&';
+
+        for (int i = 0; i < 4; i++) {
+            ch = (char) file.read();
+        }
+
+        assertEquals('d', ch);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        file.close();
+    }
+
+    private InputStream loadFixtureFile() {
+        return getClass().getClassLoader().getResourceAsStream("fixture.txt");
+    }
+}
