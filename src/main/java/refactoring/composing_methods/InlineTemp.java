eaf28dffd170fa7f9ba5e7fd81ffc0595c2344ea
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-05-28 15:16:42 -0300

    Adiciona código da refatoração

---
+++ src/main/java/refactoring/composing_methods/InlineTemp.java
@@ -0,0 +1,15 @@
+package refactoring.composing_methods;
+
+public class InlineTemp {
+    private Order anOrder;
+
+    public InlineTemp(Order order) {
+        anOrder = order;
+    }
+
+    boolean basePriceLargerThanThousand() {
+        double basePrice = anOrder.basePrice();
+
+        return basePrice > 1000;
+    }
+}
